12b319f2163638f7ad4e1c444a10bfb62872c975
LeoBarreiro <a.leo.barreiro@gmail.com>
2022-09-24 21:45:47 -0300

    neovim nightly

--- nvim.sh
+++ nvim.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-PACKAGES="neovim xclip wl-clipboard"
+PACKAGES="xclip wl-clipboard"
 ! pacman --query $PACKAGES && sudo pacman -S $PACKAGES --noconfirm
+
+# Only for winbar. On 0.8 stable i will back.
+PACKAGES="neovim-nightly-bin"
+! pacman --query $PACKAGES && yay -S $PACKAGES --noconfirm
 
 if [[ ! -d ~/.local/share/nvim/mason ]]; then
     nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'; true
